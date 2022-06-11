4a2ff598c61b59b2031de61a8d6bd8b80f14edc5
Melvin A <melvinalmonte@gmail.com>
2022-06-11 16:54:36 -0400

    deleted console log message

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
 import store from "./store";
-console.log("YOOO: ", import.meta.env)
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider>
